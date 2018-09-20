7ef901fa925e6b74817087efd708757dbf1da9cf
Mark Mersenski <mjmersenski@qg.com>
2018-09-19 19:00:01 -0500

    adding class8 notes and demos to repo

---
+++ class8/class8_notes.md
@@ -0,0 +1,11 @@
+#9/19/18#
+#AngularJS#
+- Covering AngularJS (uses javascript) & Angular (uses typescript)
+    - We are using AngularJS in this class
+    
+- MVC
+    - Model: data, objects, things (data thats retrieved when button is clicked)
+    - View: presentation layer, html/css (shows a button)
+    - Controller: business logic, works with model and view, handles requests (event handler for button)
+
+
---
+++ class8/demo.js
@@ -0,0 +1,55 @@
+// define our app "name" and "plugins" used
+var app = angular.module('todoApp', ['ngStorage']);
+
+// add a controller - used to handle requests
+// BUSINESS LOGIC ONLY
+// don't want to manipulate the dom or HTML/CSS here
+app.controller('todoCtrl', function ($scope, $localStorage) {
+    // $scope provides models to your view (and vice-versa) (kind of like a java bean)
+    //$scope.yourName = 'Joe';
+
+    // create a "model" for a list of todos
+    $scope.todos = [
+        { text: 'eat dinner', done: true },
+        { text: 'learn angularjs', done: false },
+        { text: 'sleep', done: false }
+    ];
+
+    $scope.$storage = $localStorage.$default({
+        todos: [
+            { text: 'eat dinner', done: true },
+            { text: 'learn angularjs', done: false },
+            { text: 'sleep', done: false }
+        ]
+    });
+
+    // custom filter to get completed todos
+    $scope.getCompleteTodos = function () {
+        return $scope.$storage.todos.filter(function (todo) {
+            // return true if we want to keep this item
+            return todo.done;
+        });
+    };
+
+    // custom filter to get incomplete todos
+    $scope.getIncompleteTodos = function () {
+        return $scope.$storage.todos.filter(function (todo) {
+            // return true if we want to keep this item
+            return !todo.done;
+        });
+    };
+
+    $scope.clearCompleted = function () {
+        $scope.$storage.todos = $scope.getIncompleteTodos();
+    }
+
+
+    // add a method to respond to a button click
+    $scope.addTodo = function () {
+        // $scope.newTodo will be the value from the text box
+        $scope.$storage.todos.push({ text: $scope.newTodo, done: false });
+        // update model to celar text box
+        $scope.newTodo = '';
+    }
+
+});
