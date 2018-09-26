9509bd0c0a313035120cb2e835081c0eeab25a26
Mark Mersenski <mjmersenski@qg.com>
2018-09-26 10:16:38 -0500

    dropdown is now populating with week values from json file

--- JS2/angularJSLab/lowesDemo.js
+++ JS2/angularJSLab/lowesDemo.js
@@ -4,7 +4,10 @@ var app = angular.module('lowesApp', ['ngStorage', 'ngMaterial', 'ngMessages']);
 // controller for the pdf file download view
 app.controller('pdfFileCtrl', function ($scope, $localStorage, $http) {
     // data for weeks dropdown menu
-    $scope.weeks = ["week1", "week2"];
+    //$scope.weeks = ["week1", "week2"];
+    $http.get('dataFile.json').then(function (data) {
+        $scope.weeks = data.data;
+    });
 
 });
 
