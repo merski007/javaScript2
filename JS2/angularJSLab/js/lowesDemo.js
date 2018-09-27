55cb80312a28ebe8d255edac96efee870fc237e5
Mark Mersenski <mjmersenski@qg.com>
2018-09-27 10:08:49 -0500

    added a search filter to html

--- JS2/angularJSLab/js/lowesDemo.js
+++ JS2/angularJSLab/js/lowesDemo.js
@@ -4,6 +4,11 @@ var app = angular.module('lowesApp', ['ngStorage', 'ngMaterial', 'ngMessages', '
 // path variable that can be switched one posting files to a different server
 var path = 'http://mmersenski.bitlampsites.com/js2/'
 
+// create an instance of jsZip
+//var zip = new JSZip();
+
+//zip.file("nested/hello.txt", "Hello World\n")
+
 // controller for the pdf file download view
 app.controller('pdfFileCtrl', function ($scope, $localStorage, $http) {
     // data for weeks dropdown menu
@@ -32,9 +37,15 @@ app.controller('pdfFileCtrl', function ($scope, $localStorage, $http) {
 
         // check to see if there is anything in the array
         if ($scope.selected) {
-            angular.forEach($scope.selected, function (value, key) {
-                console.log(value); // this works
-            })
+            /*
+                        angular.forEach($scope.selected, function (value, key) {
+                            console.log(value); // this works
+            
+                            // zip up the checked files
+                            //zip.file(filename, urlToPromise(url), { binary: true });
+                        })
+            */
+            //console.log($scope.selected);
         }
         else {
             // throw an alert to user
