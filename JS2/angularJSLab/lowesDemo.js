8431470c75ec1caf5dfab60c4f9b1bdcf643a09f
Mark Mersenski <mjmersenski@qg.com>
2018-09-27 06:58:30 -0500

    able to identify which checkbox is selected and have the check all / uncheck all buttons working

--- JS2/angularJSLab/lowesDemo.js
+++ JS2/angularJSLab/lowesDemo.js
@@ -1,5 +1,5 @@
 // define our app "name" and "plugins" used
-var app = angular.module('lowesApp', ['ngStorage', 'ngMaterial', 'ngMessages']);
+var app = angular.module('lowesApp', ['ngStorage', 'ngMaterial', 'ngMessages', 'checklist-model']);
 
 // path variable that can be switched one posting files to a different server
 var path = 'http://mmersenski.bitlampsites.com/js2/'
@@ -10,6 +10,21 @@ app.controller('pdfFileCtrl', function ($scope, $localStorage, $http) {
     $http.get('dataFile.json').then(function (data) {
         $scope.weeks = data.data;
     });
+
+    //create a blank array to store selected objects.
+    $scope.selected = {
+        files: []
+    };
+
+    // check all the boxes
+    $scope.checkAll = function () {
+        $scope.selected.files = angular.copy($scope.selectedWeek.pdfFiles);
+    };
+
+    // uncheck all the boxes
+    $scope.uncheckAll = function () {
+        $scope.selected.files = [];
+    };
 
     // download files function
     $scope.download = function () {
