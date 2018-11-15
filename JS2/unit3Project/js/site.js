1e241655ab2542a62695f5ac08446a1303ee50fb
Mark Mersenski <mjmersenski@qg.com>
2018-11-15 11:55:39 -0600

    default todos can be moved from completed to incompleted.

--- JS2/unit3Project/js/site.js
+++ JS2/unit3Project/js/site.js
@@ -1,5 +1,39 @@
 var app = angular.module('todoApp', []);
 
 app.controller('todoCtrl', function ($scope) {
-    $scope.greeting = 'This is where the magic happens :)';
+    // default array for todo list
+    $scope.todos = [
+        { text: 'eat dinner', done: true },
+        { text: 'learn angularjs', done: false },
+        { text: 'sleep', done: false }
+    ];
+
+    // custom filter to get completed todos
+    $scope.getCompleteTodos = function () {
+        return $scope.todos.filter(function (todo) {
+            return todo.done;
+        });
+    };
+
+    // custom filter to get incomplete todos
+    $scope.getIncompleteTodos = function () {
+        return $scope.todos.filter(function (todo) {
+            return !todo.done;
+        });
+    };
+
+    // clear completed todos
+    $scope.clearCompleted = function () {
+        // resets todos array value so only incomplete tasks are in there (uses the custom filter)
+        $scope.todos = $scope.getIncompleteTodos();
+    }
+
+    // add a new todo
+    $scope.addTodo = function () {
+        // $scope.newTodo will be the value from the text box
+        $scope.todos.push({ text: $scope.newTodo, done: false });
+        // update model to clear text box
+        $scope.newTodo = '';
+    }
+
 });
