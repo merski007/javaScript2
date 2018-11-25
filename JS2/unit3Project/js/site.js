fa93c2da5cde3162e66a29528022e065426c5154
Mark Mersenski <mjmersenski@qg.com>
2018-11-25 17:35:23 -0600

    user login form is working as expected

--- JS2/unit3Project/js/site.js
+++ JS2/unit3Project/js/site.js
@@ -10,67 +10,6 @@ app.run(function ($rootScope) {
     }
 });
 
-app.controller('appCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.toggleLeft = buildDelayedToggler('left');
-    $scope.toggleRight = buildToggler('right');
-    $scope.isOpenRight = function () {
-        return $mdSidenav('right').isOpen();
-    };
-
-    /**
-     * Supplies a function that will continue to operate until the
-     * time is up.
-     */
-    function debounce(func, wait, context) {
-        var timer;
-
-        return function debounced() {
-            var context = $scope,
-                args = Array.prototype.slice.call(arguments);
-            $timeout.cancel(timer);
-            timer = $timeout(function () {
-                timer = undefined;
-                func.apply(context, args);
-            }, wait || 10);
-        };
-    }
-
-    /**
-     * Build handler to open/close a SideNav; when animation finishes
-     * report completion in console
-     */
-    function buildDelayedToggler(navID) {
-        return debounce(function () {
-            // Component lookup should always be available since we are not using `ng-if`
-            $mdSidenav(navID)
-                .toggle()
-                .then(function () {
-                    $log.debug("toggle " + navID + " is done");
-                });
-        }, 200);
-    }
-
-    function buildToggler(navID) {
-        return function () {
-            // Component lookup should always be available since we are not using `ng-if`
-            $mdSidenav(navID)
-                .toggle()
-                .then(function () {
-                    $log.debug("toggle " + navID + " is done");
-                });
-        };
-    }
-});
-
-app.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav('right').close()
-            .then(function () {
-                $log.debug("close RIGHT is done");
-            });
-    };
-});
 
 app.controller('todoCtrl', function ($scope, $firebaseArray, $firebaseAuth) {
     // init firebase
@@ -167,10 +106,36 @@ app.controller('todoCtrl', function ($scope, $firebaseArray, $firebaseAuth) {
 });
 
 
-app.controller('authCtrl', function ($scope, $firebaseAuth, $firebaseObject) {
-    // right side nav
-    $scope.openRightMenu = function () {
-        $mdSidenav('left').toggle();
+app.controller('authCtrl', function ($scope, $firebaseAuth, $firebaseObject, $mdSidenav, $log) {
+    // right side nav functions
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function () {
+        return $mdSidenav('right').isOpen();
+    };
+
+    function buildToggler(navID) {
+        return function () {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+                .toggle()
+                .then(function () {
+                    $log.debug("toggle " + navID + " is done");
+                });
+
+            // clear out user input fields and reset form field condition 
+            $scope.userLogin.$setPristine();
+            $scope.userLogin.$setUntouched();
+            $scope.userEmail = '';
+            $scope.userPassword = '';
+        };
+    }
+
+    $scope.close = function () {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav('right').close()
+            .then(function () {
+                $log.debug("close RIGHT is done");
+            });
     };
 
     // initalize database
@@ -189,6 +154,7 @@ app.controller('authCtrl', function ($scope, $firebaseAuth, $firebaseObject) {
             console.log("Signed in as: auth state change", firebaseUser.uid);
             $scope.authUser = firebaseUser;
             $scope.user = $firebaseObject(app.firebaseRef.child('users').child($scope.authUser.uid));
+            $scope.close();
         } else {
             console.log("Signed out");
             $scope.authUser = null;
@@ -207,6 +173,7 @@ app.controller('authCtrl', function ($scope, $firebaseAuth, $firebaseObject) {
         $scope.authObj.$createUserWithEmailAndPassword($scope.userEmail, $scope.userPassword)
             .then(function (firebaseUser) {
                 console.log("User " + firebaseUser.uid + " created successfully!");
+                $scope.close();
             }).catch(function (error) {
                 console.error("Error: ", error);
             });
@@ -220,6 +187,7 @@ app.controller('authCtrl', function ($scope, $firebaseAuth, $firebaseObject) {
         // login - pass username and password to this method
         $scope.authObj.$signInWithEmailAndPassword($scope.userEmail, $scope.userPassword).then(function (firebaseUser) {
             console.log("Signed in as:", firebaseUser.uid);
+            $scope.close();
         }).catch(function (error) {
             console.error("Authentication failed:", error);
         });
